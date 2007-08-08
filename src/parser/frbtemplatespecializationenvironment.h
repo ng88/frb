0265db0be8db3ade2f6f79acaee0cd8db79eb09d
ng <nicolas@nghs.fr>
2007-08-08 10:26:05 +0000

    added frbtemplatespecializationenvironment class
    git-svn-id: file:///Users/ng/svn-local/to_share/frb/trunk@249 f04dad46-9b18-0410-952a-bb25a776dc73

---
+++ src/parser/frbtemplatespecializationenvironment.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   This file is part of the FrB project                                  *
+ *                                                                         *
+ *   FrB project                                                           *
+ *                                                                         *
+ *   Copyright (C) 2006, 2007 by GUILLAUME Nicolas                         *
+ *   ng@ngsoft-fr.com                                                      *
+ *                                                                         *
+ *   http://frb.ngsoft-fr.com/                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License only.          *
+ *   See the COPYING file.                                                 *
+ ***************************************************************************/
+
+
+#ifndef FRBTEMPLATESPECIALIZATIONENVIRONMENT_H
+#define FRBTEMPLATESPECIALIZATIONENVIRONMENT_H
+
+#include "frbfunction.h"
+class FrBClass;
+
+
+class FrBTemplateSpecializationEnvironment
+{
+
+private:
+
+    FrBConstClassVector _args;
+    FrBCodeClass *      _currentClass;
+    FrBMember *         _currentMember;
+    
+public:
+
+    inline void setCurrentClass(const FrBCodeClass * v) { _currentClass = v; }
+    inline FrBCodeClass * currentClass() const { return _currentClass; }
+
+    inline void setCurrentMember(const FrBMember * v) { _currentMember = v; }
+    inline FrBMember * currentMember() const { return _currentMember; }
+
+
+};
+
+
+#endif
