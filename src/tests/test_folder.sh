35d2571fc9a72c32bcb4b2c7bdc3ad960950992a
ng <nicolas@nghs.fr>
2007-09-02 21:02:37 +0000

    added no regression tests
    git-svn-id: file:///Users/ng/svn-local/to_share/frb/trunk@270 f04dad46-9b18-0410-952a-bb25a776dc73

--- src/tests/test_folder.sh
+++ src/tests/test_folder.sh
@@ -21,11 +21,33 @@
 # for each .frb file in the directory, a .frb.result file
 # must exists (containing the expected result)
 
-if ! [ -d $1 ]
+
+
+# USAGE
+# type ./test_folder noregression to execute the no regression tests
+#
+# to create your own test, juste put a .frb file in a directory and run
+# the following commands :
+#
+#    $FRB_PATH --file YOUR_FILE.frb > YOUR_FILE.frb.result 2>&1
+#    sed -i -e 's/0x[0-9a-fA-F][0-9a-fA-F]*/@ADDR@/g YOUR_FILE.frb.result
+#
+
+
+[ "$FRB_PATH" = "" ] && FRB_PATH=../parser/parser_test
+
+if ! [ -x "$FRB_PATH" ]
 then
-    echo usage: $0 folder >&2
+    echo "unable to execute FrB ($FRB_PATH), please set the FRB_PATH environment variable." >&2
     exit 1
 fi
+
+if [ "$1" = "" -o ! -d "$1" ]
+then
+    echo usage: $0 folder >&2
+    exit 2
+fi
+
 
 cd $1
 
@@ -33,8 +55,20 @@ for i in *.frb
 do
     if [ -r $i -a -r $i.result ]
     then
+	echo -n "Test $i: "
+	cd - > /dev/null
+	"$FRB_PATH" --file $1/$i 2>&1 | sed -e 's/0x[0-9a-fA-F][0-9a-fA-F]*/@ADDR@/g' | diff -u - $1/$i.result
+	if [ $? -eq 0 ]
+	then
+	    echo success
+	else
+	    echo FAILED
+	    exit 3
+	fi
+	cd - > /dev/null
     else
         echo "warning: can't read $1/$i or $1/$i.result" >&2
     fi
 done
 
+echo Done.
